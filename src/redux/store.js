88f004dc42c68c7ccb532a1cdea71d7c1caae21b
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-28 01:48:34 +0200

    Connected usersAPI slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,17 @@ import { authReducer } from './authSlice.js';
 
 import { phonebookAPI } from './contactsApiSlice.js';
 import { filterReducer } from './filterSlice';
+import { usersAPI } from './usersAPISlice.js';
 
 export const store = configureStore({
   reducer: {
     [phonebookAPI.reducerPath]: phonebookAPI.reducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
     filter: filterReducer,
     auth: authReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(phonebookAPI.middleware),
+    getDefaultMiddleware()
+      .concat(phonebookAPI.middleware)
+      .concat(usersAPI.middleware),
 });
