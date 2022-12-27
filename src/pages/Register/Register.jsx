4509227c9f4846fb7b2d08023442dc2ab67b63dd
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-27 22:52:09 +0200

    Added base component markup and rough register logic

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,36 @@
+import { useSignupUserMutation } from 'redux/usersAPISlice';
+
 export function Register() {
-  return null;
+  const [sendSignupRequest, { isLoading }] = useSignupUserMutation();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const {
+      name: { value: name },
+      email: { value: email },
+      password: { value: password },
+    } = e.target.elements;
+
+    sendSignupRequest({ name, email, password });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        Name
+        <input type="text" name="name" autoComplete="on" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" autoComplete="on" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" minLength="5" maxLength="12" />
+      </label>
+      <button type="submit" disabled={isLoading}>
+        Register me
+      </button>
+    </form>
+  );
 }
