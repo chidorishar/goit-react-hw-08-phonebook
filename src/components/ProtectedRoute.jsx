216f9d1094690acf465ce2d54e34e5f7b40d10e1
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-28 20:48:26 +0200

    Added route wrappers that deal with user access to pages

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { PropTypes } from 'prop-types';
+
+import { useAuth } from 'hooks';
+import { Navigate } from 'react-router-dom';
+
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+RestrictedRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  redirectTo: PropTypes.string,
+};
