5204c2014700abd106ff6e08421d3e08a241d84d
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-28 02:06:38 +0200

    Added rough Login component markup and a user request logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,42 @@
+import { Notify } from 'notiflix';
+
+import { useLoginUserMutation } from 'redux/usersAPISlice';
+
 export function Login() {
-  return null;
+  const [sendLoginRequest, { isLoading }] = useLoginUserMutation();
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = e.target.elements;
+    const userCredentials = { email, password };
+
+    try {
+      const {
+        user: { name: userName },
+      } = await sendLoginRequest(userCredentials).unwrap();
+      Notify.success(`You has been successfully login! Welcome ${userName} 🥳`);
+    } catch (err) {
+      console.log(err);
+      Notify.failure(`Login failed! Please try again.`);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        Email
+        <input type="email" name="email" autoComplete="on" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" minLength="5" maxLength="12" />
+      </label>
+      <button type="submit" disabled={isLoading}>
+        Login me!
+      </button>
+    </form>
+  );
 }
