519d761c7baf4445203e788ac289b036ce821045
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-28 20:57:12 +0200

    Implemented restricted routes in main routing logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,22 @@ import SharedLayout from './SharedLayout/SharedLayout';
 import { Contacts } from 'pages/Contacts/Contacts';
 import { Login } from 'pages/Login/Login';
 import { Register } from 'pages/Register/Register';
+import { RestrictedRoute } from './ProtectedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="contacts" element={<Contacts />} />
+        <Route index element={<RestrictedRoute component={<Register />} />} />
+        <Route
+          path="login"
+          element={<RestrictedRoute component={<Login />} />}
+        />
+        <Route
+          path="contacts"
+          element={<PrivateRoute component={<Contacts />} />}
+        />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
