9f17b8c20a82f61b277a074394181597210631e2
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-29 23:49:33 +0200

    Fixed mistakes which caused immer's errors while refreshing user data

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -39,18 +39,14 @@ const authSlice = createSlice({
     builder
       .addMatcher(signupUser.matchFulfilled, setAuthData)
       .addMatcher(loginUser.matchFulfilled, setAuthData)
-      .addMatcher(
-        refreshUser.matchPending,
-        state => (state.isRefreshingUserData = true)
-      )
-      .addMatcher(refreshUser.matchFulfilled, (state, payload) => {
-        state.isRefreshingUserData = false;
-        setAuthData(state, payload);
+      .addMatcher(refreshUser.matchFulfilled, (state, { payload }) => {
+        const transformedResp = {
+          token: state.token,
+          user: { name: payload.name, email: payload.email },
+        };
+
+        setAuthData(state, { payload: transformedResp });
       })
-      .addMatcher(
-        refreshUser.matchRejected,
-        state => (state.isRefreshingUserData = false)
-      )
       .addMatcher(logoutUser.matchFulfilled, clearAuthData);
   },
 });
