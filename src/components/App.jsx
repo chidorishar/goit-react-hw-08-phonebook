13e89d462f12ef276d56c569603649bdcded00de
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-12-29 23:59:18 +0200

    Implemented refreshing user data using a persisted token on app init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { useRefreshUserQuery } from 'redux/slices/usersAPISlice';
+
 import SharedLayout from './SharedLayout/SharedLayout';
 import { Contacts } from 'pages/Contacts/Contacts';
 import { Login } from 'pages/Login/Login';
@@ -8,25 +10,36 @@ import { RestrictedRoute } from './ProtectedRoute';
 import { PrivateRoute } from './PrivateRoute';
 
 export function App() {
+  const { isLoading: isRefreshingUserData } = useRefreshUserQuery();
+
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route
-          index
-          element={
-            <RestrictedRoute redirectTo="contacts" component={<Register />} />
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <RestrictedRoute redirectTo="contacts" component={<Login />} />
-          }
-        />
-        <Route
-          path="contacts"
-          element={<PrivateRoute component={<Contacts />} />}
-        />
+        {isRefreshingUserData ? (
+          <Route index element={<p>Retrieving data...</p>} />
+        ) : (
+          <>
+            <Route
+              index
+              element={
+                <RestrictedRoute
+                  redirectTo="contacts"
+                  component={<Register />}
+                />
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <RestrictedRoute redirectTo="contacts" component={<Login />} />
+              }
+            />
+            <Route
+              path="contacts"
+              element={<PrivateRoute component={<Contacts />} />}
+            />
+          </>
+        )}
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
